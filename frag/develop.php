ee5991bf62290261de4b25545e94cb70fa0e341d
Xi-Plus <a2093064@yahoo.com.tw>
2014-10-16 20:11:37 +0800

    <a target="_blank" href="">

--- frag/develop.php
+++ frag/develop.php
@@ -1,6 +1,7 @@
-<div style = "width: 100%">
 
+<div style = "width: 100%">
 <center>
+
 <div style = "width: 800px">
 
 
@@ -19,10 +20,10 @@
                 <div style = "text-align: right">
                 <!--<a title = "homepage" href = "" class = 'icon' style = "margin: 0 4px">&#xe817;</a>-->
                 <!--<a title = "facebook" href = "" class = 'icon' style = "margin: 0 4px">&#xe845;</a>-->
-                <a title = "twitter" href = "http://twitter.com/_JohnLallo" class = 'icon' style = "margin: 0 4px" target="new">&#xe844;</a> 
-                <a title = "nuff97@gmail.com" href = "mailto:nuff97@gmail.com" class = 'icon' style = "margin: 0 4px" target="new">&#xe850;</a>
-                <a title = "tumblr" href = "http://nuffthemighty.tumblr.com" class = 'icon' style = "margin: 0 4px" target="new">&#xe843;</a> 
-                <a title = "github"href = "http://github.com/john-lallo" class = 'icon' style = "margin: 0 4px" target="new">&#xe809;</a>
+                <a title = "twitter" href = "http://twitter.com/_JohnLallo" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe844;</a> 
+                <a title = "nuff97@gmail.com" href = "mailto:nuff97@gmail.com" class = 'icon' style = "margin: 0 4px">&#xe850;</a>
+                <a title = "tumblr" href = "http://nuffthemighty.tumblr.com" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe843;</a> 
+                <a title = "github"href = "http://github.com/john-lallo" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe809;</a>
                 </div>
                 
             </div>
@@ -39,16 +40,16 @@
                 breaking everything he touches in order to force
                 the fixing of bugs.<br><br>
                 Domen is curretly the 16<sub>th</sub> leader of TFcis Club and
-                manages <a href = "https://tfcis.org">tfcis.org</a>.<br><br>
+                manages <a href = "http://tfcis.org" target="_blank">tfcis.org</a>.<br><br>
                 
                 <div style = "text-align: right">
-                <a title = "homepage" href = "http://sites.google.com/site/domenprg/" class = 'icon' style = "margin: 0 4px" target="new">&#xe817;</a>
-                <a title = "facebook" href = "https://www.facebook.com/DomenSu111" class = 'icon' style = "margin: 0 4px" target="new">&#xe845;</a>
+                <a title = "homepage" href = "http://sites.google.com/site/domenprg/" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe817;</a>
+                <a title = "facebook" href = "https://www.facebook.com/DomenSu111" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe845;</a>
                 <!--<a title = "twitter" href = "" class = 'icon' style = "margin: 0 4px">&#xe844;</a>-->
-                <a title = "domen111@tfcis.org" href = "mailto:domen111@tfcis.org" class = 'icon' style = "margin: 0 4px" target="new">&#xe850;</a>
-                <a title = "domen.public@gmail.com" href = "mailto:domen.public@gmail.com" class = 'icon' style = "margin: 0 4px" target="new">&#xe850;</a>
+                <a title = "domen111@tfcis.org" href = "mailto:domen111@tfcis.org" class = 'icon' style = "margin: 0 4px">&#xe850;</a>
+                <a title = "domen.public@gmail.com" href = "mailto:domen.public@gmail.com" class = 'icon' style = "margin: 0 4px">&#xe850;</a>
                 <!--<a title = "tumblr" href = "" class = 'icon' style = "margin: 0 4px">&#xe843;</a> -->
-                <a title = "github"href = "http://github.com/domen111" class = 'icon' style = "margin: 0 4px" target="new">&#xe809;</a>
+                <a title = "github"href = "http://github.com/domen111" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe809;</a>
                 </div>
                 
             </div>
@@ -68,11 +69,11 @@
                 he's probably just trying to find out across how far a distance would bluetooth still work.
                 <div style = "text-align: right">
                 <!--<a title = "homepage" href = "" class = 'icon' style = "margin: 0 4px">&#xe817;</a>-->
-                <a title = "facebook" href = "http://www.facebook.com/profile.php?id=100005870494945" class = 'icon' style = "margin: 0 4px" target="new">&#xe845;</a>
+                <a title = "facebook" href = "http://www.facebook.com/profile.php?id=100005870494945" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe845;</a>
                 <!--<a title = "twitter" href = "" class = 'icon' style = "margin: 0 4px">&#xe844;</a> -->
                 <!--<a title = "" href = "" class = 'icon' style = "margin: 0 4px">&#xe850;</a>-->
                 <!--<a title = "tumblr" href = "" class = 'icon' style = "margin: 0 4px">&#xe843;</a>-->
-                <a title = "github"href = "http://github.com/xi-plus" class = 'icon' style = "margin: 0 4px" target="new">&#xe809;</a>
+                <a title = "github" href = "http://github.com/xi-plus" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe809;</a>
                 </div>
                 
             </div>
@@ -86,15 +87,15 @@
                 <strong>LFsWang</strong><br>
                 Supervises the progress of the project.<br><br>
                 He is currently working on a much more ambitious project,
-                the <a href = "https://github.com/TFcis/SkyOnlinejudge">Sky Online Judge</a>.<br><br>
+                the <a href = "http://www.tfcis.org/Toj" target="_blank">Sky Online Judge</a>.<br><br>
                 
                 <div style = "text-align: right">
                 <!--<a title = "homepage" href = "" class = 'icon' style = "margin: 0 4px">&#xe817;</a>-->
-                <a title = "facebook" href = "https://www.facebook.com/lfswang" class = 'icon' style = "margin: 0 4px">&#xe845;</a>
+                <a title = "facebook" href = "https://www.facebook.com/lfswang" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe845;</a>
                 <!--<a title = "twitter" href = "" class = 'icon' style = "margin: 0 4px">&#xe844;</a>-->
                 <!--<a title = "" href = "" class = 'icon' style = "margin: 0 4px">&#xe850;</a>-->
                 <!--<a title = "tumblr" href = "http://nuffthemighty.tumblr.com" class = 'icon' style = "margin: 0 4px">&#xe843;</a> -->
-                <a title = "github"href = "http://github.com/LFsWang" class = 'icon' style = "margin: 0 4px" target="new">&#xe809;</a>
+                <a title = "github"href = "http://github.com/LFsWang" class = 'icon' style = "margin: 0 4px" target="_blank">&#xe809;</a>
                 </div>
                 
             </div>
@@ -109,22 +110,22 @@
         <p>
         <strong>EchoStats</strong> is an open-source project
         initiated and maintained by the merry folk at the 
-        <a href = "http://tfcis.org">TNFSH Computer & Information Science club</a>,
+        <a href = "http://tfcis.org" target="_blank">TNFSH Computer & Information Science club</a>,
         years 104<sub>th</sub> & 105<sub>th</sub>.
         </p>
         
         <p>
         The source code of the entire project (including website & backend),
         as well as instructions to setting up the system on your own machine,
-        can be found on the <a href = "https://github.com/TFcis/stats-dev"> GitHub <span class = 'icon'>&#xe813;</span> repo</a>.
+        can be found on the <a href = "https://github.com/TFcis/stats-dev" target="_blank"> GitHub <span class = 'icon' target="_blank">&#xe813;</span> repo</a>.
         </p>
 
         <p>Development versions of the site are also hosted at various locations:</p>
         
-        <a href = "https://john-lallo.iiiii.info/stats-dev">Koding VM</a>
-        <br>
+        <!--<a href = "https://john-lallo.iiiii.info/stats-dev" target="_blank">Koding VM</a>
+        <br>-->
         
-        <a href = "https://tfcis.org/problemlist_test/">tfcis.org</a>
+        <a href = "http://tfcis.org/problemlist_test/" target="_blank">tfcis.org</a>
         <br>
         
         <p>Those interested are welcome to follow our development or to contribute by forking the repository.</p>
@@ -132,6 +133,6 @@
     
     
 </div>
-<center>
+</center>
 
 </div>
