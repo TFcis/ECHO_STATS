7868df1b1312953ca87d3e8ef5180056ece46d53
Domen <domen.public@gmail.com>
2014-10-17 12:59:18 +0800

    Admin page iframe auto height

--- frag/konfigurator.php
+++ frag/konfigurator.php
@@ -1,2 +1,12 @@
-<iframe src="./frag/admin.php" frameborder="0" border="0" cellspacing="0" style="border:none" width="100%" height="600px">
+<iframe src="./frag/admin.php" frameborder="1" border="0" cellspacing="0" style="border:none" width="100%" height="100%">
 </iframe>
+
+<script>
+window.onload = function() {
+	$(window).resize(windowSizeChange);
+	windowSizeChange();
+}
+function windowSizeChange(){
+	$("#content").height( $(window).height() - $("#content").position().top - 10 );
+}
+</script>
