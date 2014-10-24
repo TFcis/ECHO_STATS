a82347ce292d4c689535b685be3c7f03b89c5bb8
domen111 <domen.public@gmail.com>
2014-10-24 05:23:50 +0000

    Clone Setting Function

--- frag/admin.php
+++ frag/admin.php
@@ -30,6 +30,7 @@ function echoAdminPage(){  ?>
 <?php
 $files = array("groups","names","probs");
 if(isset($_POST[$files[0]])){
+    echo "edit ";
 	foreach($files as $file){
 		$content = $_POST[$file];
 		if($file == "probs"){
@@ -41,7 +42,24 @@ if(isset($_POST[$files[0]])){
 		if(@file_put_contents("../config/$file.dat",$content) === false)
 			echo "Failed to write file: $file. Please check file permission.<br/>";
 	}
-	echo "done<br/>";
+	echo "done ".time()."<br/>";
+}else if(isset($_POST["url"])){
+    $url = $_POST["url"];
+    $url = explode("#", $url)[0];
+    $url = explode("index.php", $url)[0];
+    if(substr($url, -1)!="/")
+        $url .= "/";
+    $url .= "config/";
+    echo "clone setting from $url  ";
+	foreach($files as $file){
+	    $content = "";
+	    $dataUrl = $url.$file.".dat";
+        if(($content = @file_get_contents("http://www.tfcis.org/MY_STATS/config/groups.dat")) === false)
+            echo "Unable to fetch data from: ".$dataUrl."<br/>";
+        else if(@file_put_contents("../config/$file.dat",$content) === false)
+			echo "Failed to write file: $file. Please check file permission.<br/>";
+	}
+	echo "done ".time()."<br/>";
 }
 ?>
 
@@ -53,7 +71,7 @@ if(isset($_POST[$files[0]])){
 </style>
 
 <form method="POST">
-<input type="submit"><br/>
+<input type="submit" value="Submit"><br/>
 <?php
 foreach($files as $file){
 ?>
@@ -63,6 +81,16 @@ foreach($files as $file){
 </textarea><br/>
 <?php } ?>
 <input type="hidden" name="pwd" value="<?=$_POST["pwd"]?>">
+</form>
+
+<br/><br/>
+
+Clone setting from another ECHO_STATS:
+<form method="POST">
+<input type="text" name="url" placeholder="ECHO_STATS URL">
+<input type="submit" value="Download">
+<input type="hidden" name="pwd" value="<?=$_POST["pwd"]?>">
+<br/>Waring: It will replace all the setting data here.
 </form>
 
 
