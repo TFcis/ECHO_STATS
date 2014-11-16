699cee369b31ff569319df1cafd45e03b3844a46
Xi-Plus <a2093064@yahoo.com.tw>
2014-11-16 15:59:38 +0800

    [Dev] koding不能用cURL?

--- func/curl_get_contents.php
+++ func/curl_get_contents.php
@@ -1,5 +1,5 @@
 <?php
-function curl_get_contents($url,$timeout){ 
+/*function curl_get_contents($url,$timeout){ 
 	$curlHandle = curl_init(); 
 	curl_setopt( $curlHandle , CURLOPT_URL, $url ); 
 	curl_setopt( $curlHandle , CURLOPT_RETURNTRANSFER, 1 ); 
@@ -7,5 +7,15 @@ function curl_get_contents($url,$timeout){
 	$result = curl_exec( $curlHandle ); 
 	curl_close( $curlHandle ); 
 	return $result; 
-} 
+} */
+function curl_get_contents($url,$timeout){
+	$ctx = stream_context_create(
+    	array(
+            'http' => array(
+                'timeout' => $timeout/1000
+            )
+        )
+    ); 
+    return file_get_contents($url, 0, $ctx); 
+}
 ?>
