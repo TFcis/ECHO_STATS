d6cb70904893fbc75e4d42983fd6834bcb4e5b20
Xi-Plus <a2093064@yahoo.com.tw>
2014-11-16 14:44:20 +0800

    color SE

--- frag/board.php
+++ frag/board.php
@@ -126,4 +126,7 @@
     
 </div>
 <script type='text/javascript' src='./func/edit_font_color.js'></script>
-<script>dfs(document.all.content);</script>
+<script>
+document.all.color.value=(getCookie("color")==""?"#ffffff":getCookie("color"));
+dfs(document.all.content);
+</script>
