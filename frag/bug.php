dcb1ee4e6a0ae84cef8058fc4a2b098173a676b3
Xi-Plus <a2093064@yahoo.com.tw>
2014-11-17 07:56:54 +0800

    Revert "SE"
    This reverts commit ef118f718cc6872d3b3baa3e271d21d3ab3af15f.

--- frag/bug.php
+++ frag/bug.php
@@ -1,6 +1,1 @@
 PLACEHOLDER: BUG REPORT PAGE
-<script type='text/javascript' src='./func/edit_font_color.js'></script>
-<script>
-document.all.color.value=(getCookie("color")==""?"#ffffff":getCookie("color"));
-dfs(document.all.content);
-</script>
