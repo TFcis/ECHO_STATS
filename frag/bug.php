ef118f718cc6872d3b3baa3e271d21d3ab3af15f
Xi-Plus <a2093064@yahoo.com.tw>
2014-11-17 07:56:20 +0800

    SE

--- frag/bug.php
+++ frag/bug.php
@@ -1,1 +1,6 @@
 PLACEHOLDER: BUG REPORT PAGE
+<script type='text/javascript' src='./func/edit_font_color.js'></script>
+<script>
+document.all.color.value=(getCookie("color")==""?"#ffffff":getCookie("color"));
+dfs(document.all.content);
+</script>
