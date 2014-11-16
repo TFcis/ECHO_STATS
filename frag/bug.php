fdb70cc6dfc156c7a7fe6a5377ab7e7cfb330aa8
Xi-Plus <a2093064@yahoo.com.tw>
2014-11-17 07:38:51 +0800

    SE

--- frag/bug.php
+++ frag/bug.php
@@ -1,3 +1,6 @@
 PLACEHOLDER: BUG REPORT PAGE
 <script type='text/javascript' src='./func/edit_font_color.js'></script>
-<script>dfs(document.all.content);</script>
+<script>
+dfs(document.all.content);
+editcolor(document.all.content,color_R,color_G,color_B);
+</script>
